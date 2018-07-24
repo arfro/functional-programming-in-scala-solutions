170a4a887962ac661c2d4ce88406fd076dcd598c
arfro <aerfromenes@gmail.com>
2018-07-24 16:58:33 +0100

    added 3.4

--- src/main/scala/partone/chapterthree/List.scala
+++ src/main/scala/partone/chapterthree/List.scala
@@ -6,6 +6,10 @@ case class Cons[+A](head: A, tail: CustomList[A]) extends CustomList[A]
 
 object CustomList{
 
+  def apply[A](as: A*): CustomList[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
   //exercise 3.2
   def removeTail[A](list: CustomList[A]): CustomList[A] = {
     list match {
@@ -13,4 +17,25 @@ object CustomList{
       case Cons(_, tail) => tail
     }
   }
+
+  //exercise 3.3
+  def setHead[A](head: A, list: CustomList[A]): CustomList[A] = {
+    list match {
+      case Nil => Nil
+      case Cons(_, tail) => Cons(head, tail)
+    }
+  }
+
+  //exercise 3.4
+  def drop[A](list: CustomList[A], n: Int): CustomList[A] = {
+    if (n == 0) list
+    else
+    list match {
+      case Nil => Nil
+      case Cons(_, tail) if (n > 1) => drop(removeTail(list), n-1)
+      case Cons(_, tail) if (n <= 1) => tail
+    }
+  }
+
+
 }
--- src/test/scala/partone/chapterthree/ListSpec.scala
+++ src/test/scala/partone/chapterthree/ListSpec.scala
@@ -17,4 +17,28 @@ class ListSpec extends FlatSpec with Matchers {
     CustomList.removeTail(Cons(1, Cons(2, Nil))) shouldBe Cons(2, Nil)
   }
 
+  "List setHead" should "replace head with 3" in {
+    CustomList.setHead(3, Cons(1, Cons(3, Nil))) shouldBe Cons(3, Cons(3, Nil))
+  }
+
+  "List drop" should "drop first two elements" in {
+    CustomList.drop(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), 2) shouldBe Cons(3, Cons(4, Nil))
+  }
+
+  "List drop" should "drop first element" in {
+    CustomList.drop(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), 1) shouldBe Cons(2, Cons(3, Cons(4, Nil)))
+  }
+
+  "List drop" should "handle empty list" in {
+    CustomList.drop(Nil, 3) shouldBe Nil
+  }
+
+  "List drop" should "drop all if drop number larger than list length" in {
+    CustomList.drop(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), 9) shouldBe Nil
+  }
+
+  "List drop" should "not drop anything is drop number is zero" in {
+    CustomList.drop(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), 0) shouldBe Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+  }
+
 }
