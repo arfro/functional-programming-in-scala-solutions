1af33bb4c96e516f9b32b2b13c8886aa254ecc00
arfro <aerfromenes@gmail.com>
2018-07-24 15:02:41 +0100

    added exercises 2.3, 2.4, 2.5

---
+++ src/main/scala/partone/chaptertwo/Curry.scala
@@ -0,0 +1,22 @@
+package partone.chaptertwo
+
+object Currying {
+
+  //exercise 2.3
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
+  }
+
+  //exercise 2.4
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  //exercise 2.5
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
+}
