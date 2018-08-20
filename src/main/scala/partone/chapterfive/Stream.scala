2e8a21a0538317a2607fe3a66377e5c329b0424f
arfro <aerfromenes@gmail.com>
2018-08-20 13:33:47 +0100

    excercise 5.2 and 5.3

--- src/main/scala/partone/chapterfive/Stream.scala
+++ src/main/scala/partone/chapterfive/Stream.scala
@@ -1,22 +1,54 @@
 package partone.chapterfive
 
+import partone.chapterfive.Stream.{cons, empty}
+
+import scala.annotation.tailrec
+
 sealed trait Stream[+A] {
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, _) => Some(h())
+  }
+
+  // exercise 5.1
+  def toList: List[A] = this match {
+    case Empty => List()
+    case Cons(h, t) => h() :: t().toList
+  }
+
+
+  //exercise 5.2
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+  //exercise 5.2
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
+
+  //exercise 5.3
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+    case _ => Empty
   }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
+  // smart constructor
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
+  // smart constructor
   def empty[A]: Stream[A] = Empty
 
+  // smart constructor
   def apply[A](as: A*): Stream[A] =
     if(as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
--- src/test/scala/partone/chapterfive/StreamSpec.scala
+++ src/test/scala/partone/chapterfive/StreamSpec.scala
@@ -4,12 +4,44 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class StreamSpec extends FlatSpec with Matchers {
 
-  "Stream" should "create new empty stream" in {
+  "Stream creating" should "create new empty stream" in {
     Stream().headOption shouldBe None
   }
 
-  "Stream" should "create new non empty stream" in {
+  it should "create new non empty stream" in {
     Stream(1,2,3).headOption shouldBe Some(1)
+  }
+
+  "Stream to list" should "return empty list for Empty Stream" in {
+    Stream().toList shouldBe List()
+  }
+
+  it should "return list 1 2 3 for Stream 1 2 3" in {
+    Stream(1,2,3).toList shouldBe List(1,2,3)
+  }
+
+  "Stream - take" should "return 1 2 for take 2 on List 123" in {
+    Stream(1,2,3).take(2).toList shouldBe List(1,2)
+  }
+
+  it should "return empty list for take 2 on empty list" in {
+    Stream().take(2).toList shouldBe List()
+  }
+
+  "Stream - drop" should "return 3 for drop 2 on List 123" in {
+    Stream(1,2,3).drop(2).toList shouldBe List(3)
+  }
+
+  it should "return empty list for drop 2 on empty list" in {
+    Stream().take(2).toList shouldBe List()
+  }
+
+  "Stream - takeWhile" should "return stream 1 2 3 with takeWhile numbers that - 3 are even or less than 0" in {
+    Stream(1,2,3,4).takeWhile(_ - 3 <= 0).toList shouldBe List(1,2,3)
+  }
+
+  it should "return empty list for empty stream" in {
+    Stream(1).drop(1).takeWhile(_%2 != 0).toList shouldBe List()
   }
 
 }
